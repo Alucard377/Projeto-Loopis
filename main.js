c317093191d1bfc00f6d8a03495f3442da9236ee
Alexis Zorba <109966871+Alucard377@users.noreply.github.com>
2023-09-20 22:40:35 -0300

    Adição da função favoritar
    Adição da função de favoritar de forma ainda não tão otimizada, adição da propriedade background para a div[className="elementos"], além de mudança da cor de azul claro para azul escuro quando favoritado. Correção de lista mudando de var para let. Automaticamente coloca a cor das tags h2 e p dos elementos para branco.

--- main.js
+++ main.js
@@ -9,23 +9,26 @@ function armazenarObjeto() {
 
     const input1 = document.getElementById("campo1");
     const input2 = document.getElementById("campo2");
+    const sim = document.querySelector('input[value="sim"]');
+    const nao = document.querySelector('input[value="nao"]');
 
-    if (input1.value && input2.value) {
-        lista.push({ x: input1.value, y: input2.value }) // Armazena objeto na lista
+    if (input1.value && input2.value &&(sim.checked || nao.checked)) {
+        lista.push({ x: input1.value, y: input2.value, z: sim.checked}) // Armazena objeto na lista
         const myObjAsJSON = JSON.stringify(lista); // Convertendo lista de objetos para string
         localStorage.jogos = myObjAsJSON; // Armazenando a lista em jogos
+        sim.checked = false;
+        nao.checked = false;
 
         // Limpando os campos
         input1.value = '';
         input2.value = '';
     } else {
-        alert("Preencha os dois campos");
+        alert("Preencha os três campos");
     }
 }
 
 function receberObejeto() {
-    const recebeString = localStorage.jogos; // Recebe a chave
-    return JSON.parse(recebeString); // Converte para objeto
+    return JSON.parse(localStorage.jogos); // Busca a chave e converte para objeto
 }
 
 function funcoesCompiladas() {
@@ -33,34 +36,70 @@ function funcoesCompiladas() {
     mostrarObejeto(receberObejeto());
 }
 
+function criarElemento(objeto){
+    const novoElemento = document.createElement("div");
+    novoElemento.className = "elemento";
+
+    const h2 = document.createElement("h2");
+    h2.style.color = "white";
+
+    const p = document.createElement("p");
+    p.style.color = "white";
+
+    h2.innerHTML = objeto.x;
+    p.innerHTML = objeto.y;
+
+    novoElemento.appendChild(h2);
+    novoElemento.appendChild(p);
+
+    const imagem = document.createElement('img'); // Criando o elemento <img>
+    imagem.className = 'lixeiro';
+    imagem.src = 'Imagens/lixeiro.png'; // Definindo o atributo src da imagem
+
+    novoElemento.appendChild(imagem);
+
+    let retiraElemento = guardarElemento(objeto);
+    novoElemento.querySelector(".lixeiro").addEventListener("click", retiraElemento);
+
+    const estrela = document.createElement('img');
+    estrela.className = 'estrela';
+    if(objeto.z){
+        estrela.src = 'Imagens/favorito_true.png';
+        novoElemento.style.background = "#10086A";
+    }
+    else{
+        estrela.src = 'Imagens/favorito_false.png';
+        novoElemento.style.background = "#3C4EFF";
+    }
+
+    let favoritaElemento = guardarFavorito(objeto);
+    
+    novoElemento.appendChild(estrela);
+
+    novoElemento.querySelector(".estrela").addEventListener("click", favoritaElemento);
+    
+
+    // console.log(novoElemento); //Retire o comentario para verificar como funciona a div da classe elemento
+
+    return novoElemento;
+}
+
 function mostrarObejeto(objeto) {
     const destino = document.getElementById("inserir");
     destino.innerHTML = "";
+    
+    for (let i in objeto) {
+        if(objeto[i].z){//Se o elemento for favorito
+            const novoElemento = criarElemento(objeto[i]);
+            document.getElementById("inserir").appendChild(novoElemento); // Adicionando o botão ao documento na div inserir
+        }
+    }
 
     for (let i in objeto) {
-        const novoElemento = document.createElement("div");
-        novoElemento.className = "elemento";
-
-        const h2 = document.createElement("h2");
-        const p = document.createElement("p");
-
-        p.innerHTML = objeto[i].x;
-        h2.innerHTML = objeto[i].y;
-
-        novoElemento.appendChild(h2);
-        novoElemento.appendChild(p);
-
-        const botao = document.createElement('button'); // Criando o elemento <button>
-        const imagem = document.createElement('img'); // Criando o elemento <img>
-        imagem.id = 'lixeiro';
-        imagem.src = 'Imagens/lixeiro.png'; // Definindo o atributo src da imagem
-        botao.appendChild(imagem); // Adicionando a imagem como filho do botão
-
-        novoElemento.appendChild(botao);
-
-        let retiraElemento = guardarElemento(objeto[i]);
-        novoElemento.querySelector("button").addEventListener("click", retiraElemento);
-        document.getElementById("inserir").appendChild(novoElemento); // Adicionando o botão ao documento na div inserir
+        if(objeto[i].z == false){//Se o elemento não for favorito
+            const novoElemento = criarElemento(objeto[i]);
+            document.getElementById("inserir").appendChild(novoElemento); // Adicionando o botão ao documento na div inserir
+        }
     }
 }
 
@@ -73,9 +112,9 @@ function guardarElemento(elemento) { // Guarda o elemento que vai ser excluído
             numFilhos = divPai.childElementCount;
         }
 
-        let listaJogos = JSON.parse(localStorage.jogos);
+        let listaJogos = receberObejeto();
         // Essa linha encontra a posição do elemento no localStorage
-        const posicao = listaJogos.findIndex(objeto => objeto.x === elemento.x && objeto.y === elemento.y);
+        let posicao = listaJogos.findIndex(objeto => objeto.x === elemento.x && objeto.y === elemento.y);
         if (listaJogos[posicao]) {
             listaJogos.splice(posicao, 1); // Retirando o elemento de listaJogos
         }
@@ -89,4 +128,18 @@ function guardarElemento(elemento) { // Guarda o elemento que vai ser excluído
         divPai.remove(); // Exclui divPai
     }
     return botaoLixeiro;
+}
+
+function guardarFavorito(elemento) { // Guarda o elemento que vai ser favoritado
+    function toqueFavorito(event) { 
+        let listaJogos = receberObejeto();
+        let posicao = listaJogos.findIndex(objeto => objeto.x === elemento.x && objeto.y === elemento.y);
+        if(listaJogos[posicao]){
+            listaJogos[posicao].z = !listaJogos[posicao].z;
+        }
+        localStorage.jogos = JSON.stringify(listaJogos);
+
+        mostrarObejeto(receberObejeto()); //Reseta a div[id="inserir"]
+    }
+    return toqueFavorito;
 }
